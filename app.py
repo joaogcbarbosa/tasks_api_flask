02b39da94b6c7fadb8ee25d54ac85e179810003a
João Gabriel Barbosa <joaogcb29@gmail.com>
2023-01-30 15:50:15 -0300

    Formatação do código

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ app = Flask(__name__)
 
 tasks = [
     {'id': 0,
-    'responsavel': 'João Gabriel Barbosa',
-    'tarefa': 'Desenvolver API',
-    'status': 'Em andamento'},
+     'responsavel': 'João Gabriel Barbosa',
+     'tarefa': 'Desenvolver API',
+     'status': 'Em andamento'},
 
     {'id': 1,
-    'responsavel': 'José Miguel Pires',
-    'tarefa': 'Desenvolver App Mobile',
-    'status': 'Pausado'},
+     'responsavel': 'José Miguel Pires',
+     'tarefa': 'Desenvolver App Mobile',
+     'status': 'Pausado'},
 
     {'id': 2,
-    'responsavel': 'Hugo Soares',
-    'tarefa': 'Desenvolver Página',
-    'status': 'Feito'}
+     'responsavel': 'Hugo Soares',
+     'tarefa': 'Desenvolver Página',
+     'status': 'Feito'}
 ]
 
 
 @app.route("/", methods=['GET'])
 def home():
     return ('<h1>Página Inicial</h1>'
-           '<h2><a href="http://127.0.0.1:5000/todas-tarefas/">Todas Tarefas</a><br></h2>'
-           '<h3>Para pesquisar(GET) ou excluir(DELETE) tarefa por ID: http://127.0.0.1:5000/tarefa/"id"/</h3>'
-           '<h3>Para inserir(POST) tarefa: http://127.0.0.1:5000/incluir-tarefa/</h3>'
-           '<h3>Para alterar(PUT) somente o status: http://127.0.0.1:5000/alterar-status/"id"/</h3>')
+            '<h2><a href="http://127.0.0.1:5000/todas-tarefas/">Todas Tarefas</a><br></h2>'
+            '<h3>Para pesquisar(GET) ou excluir(DELETE) tarefa por ID: http://127.0.0.1:5000/tarefa/"id"/</h3>'
+            '<h3>Para inserir(POST) tarefa: http://127.0.0.1:5000/incluir-tarefa/</h3>'
+            '<h3>Para alterar(PUT) somente o status: http://127.0.0.1:5000/alterar-status/"id"/</h3>')
 
 
 @app.route("/todas-tarefas/", methods=['GET'])
@@ -60,8 +60,8 @@ def tarefa(id):
             dados = tasks[id]
         except IndexError:
             mensagem = f'NÃO HÁ TAREFA VINCULADA AO ID {id}'
-            dados = {'status': 'ERRO!', 
-                    'mensagem': mensagem}
+            dados = {'status': 'ERRO!',
+                     'mensagem': mensagem}
         except Exception():
             mensagem = f'ERRO DESCONHECIDO. RECORRER AO DESENVOLVEDOR'
             dados = {'status': 'ERRO!',
